7ed23d5b880fad7d3cbc08af17a7b5dff22cd5e0
Steven Murray <steven.g.murray@asu.edu>
2022-06-02 12:03:55 -0700

    Fix hera_env -> env

--- Precamp/install_conda_envs.sh
+++ Precamp/install_conda_envs.sh
@@ -9,7 +9,7 @@ echo "Hello, you are using a ${machine} machine."
 
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-${machine}-x86_64.sh | sh
 
-curl -o hera_env.yml https://raw.githubusercontent.com/HERA-Team/CHAMP_Bootcamp/main/Precamp/hera_env.yml
+curl -o env.yml https://raw.githubusercontent.com/HERA-Team/CHAMP_Bootcamp/main/Precamp/hera_env.yml
 conda env create -f env.yml
 rm -f env.yml
 
