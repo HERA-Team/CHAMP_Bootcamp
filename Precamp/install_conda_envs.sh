e04908532525b85374993acc6c6daaf5082d02e5
Steven Murray <steven.g.murray@asu.edu>
2022-06-02 12:00:58 -0700

    Use raw files instead of html pages

--- Precamp/install_conda_envs.sh
+++ Precamp/install_conda_envs.sh
@@ -9,10 +9,10 @@ echo "Hello, you are using a ${machine} machine."
 
 curl https://repo.anaconda.com/miniconda/Miniconda3-latest-${machine}-x86_64.sh | sh
 
-curl -o hera_env.yml https://github.com/HERA-Team/CHAMP_Bootcamp/blob/main/Precamp/hera_env.yml
+curl -o hera_env.yml https://raw.githubusercontent.com/HERA-Team/CHAMP_Bootcamp/main/Precamp/hera_env.yml
 conda env create -f env.yml
 rm -f env.yml
 
-curl -o env.yml https://github.com/HERA-Team/CHAMP_Bootcamp/blob/main/Precamp/21cmfast_env.yml
+curl -o env.yml https://raw.githubusercontent.com/HERA-Team/CHAMP_Bootcamp/main/Precamp/21cmfast_env.yml
 conda env create -f env.yml
 rm -f env.yml
